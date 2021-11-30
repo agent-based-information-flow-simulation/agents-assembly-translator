c54f6a6691bb83145c7c7ae64ffc8c0a8f81b037
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-30 19:26:09 +0100

    Use improved agent representation

--- out.py
+++ out.py
@@ -8,7 +8,7 @@ class a1(spade.agent.Agent):
         super().__init__(jid, password, verify_security=False)
         self.param1 = 200
         self.param2 = normal(2138, 2137)
-        self.param3 = "enum_val1"
+        self.param3 = r.choices(["enum_val1", "enum_val2"], [100, 0])
         self.param4 = r.choices(["enum_val3", "enum_val4"], [99, 1])
         self.param5 = []
         self.param6 = []
