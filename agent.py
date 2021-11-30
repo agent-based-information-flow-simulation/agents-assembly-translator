7f470352d0e93f7995ab9ecbd9b8bb070e6e4f46
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-30 17:59:15 +0100

    Work in progress

--- agent.py
+++ agent.py
@@ -1,17 +1,20 @@
 from typing import Dict, Tuple
-from param import Param, EnumParam, FloatParam, ListParam
+from param import EnumParam, InitFloatParam, ListParam, DistNormalFloatParam
 
 
 class Agent:
     def __init__(self, name: str):
         self.name: str = name
-        self.floats: Dict[str, FloatParam] = {}
+        self.init_floats: Dict[str, InitFloatParam] = {}
+        self.dist_normal_floats: Dict[str, DistNormalFloatParam] = {}
         self.enums: Dict[str, EnumParam] = {}
         self.lists: Dict[str, ListParam] = {}
-        self._enum_values: Dict[str, Tuple[str, str]] = {}
         
-    def add_float(self, float_param: FloatParam) -> None:
-        self.floats[float_param.name] = float_param
+    def add_init_float(self, float_param: InitFloatParam) -> None:
+        self.init_floats[float_param.name] = float_param
+
+    def add_dist_normal_float(self, float_param: DistNormalFloatParam) -> None:
+        self.dist_normal_floats[float_param.name] = float_param
     
     def add_enum(self, enum_param: EnumParam) -> None:
         self.enums[enum_param.name] = enum_param
@@ -19,32 +22,31 @@ class Agent:
     def add_list(self, list_param: ListParam) -> None:
         self.lists[list_param.name] = list_param
         
-    def add_enum_value(self, name: str, value: str) -> None:
-        self._enum_values[name] = (name, value)
-        
-    def get_param(self, name: str) -> Param:
-        if name in self.floats:
-            return self.floats[name]
-        elif name in self.enums:
-            return self.enums[name]
-        elif name in self.lists:
-            return self.lists[name]
+    # def get_param(self, name: str) -> Param:
+    #     if name in self.floats:
+    #         return self.floats[name]
+    #     elif name in self.enums:
+    #         return self.enums[name]
+    #     elif name in self.lists:
+    #         return self.lists[name]
 
     def param_exists(self, name: str) -> bool:
-        if name in self.floats or name in self.enums or name in self.lists:
-            return True
+        # if name in (*list(self.init_floats), *list(self.dist_normal_floats), *list(self.enums), *list(self, self.lists)):
+        #     return True
         return False
     
-    def get_enum_value(self, name: str) -> Tuple[str, str]:
-        return self._enum_values[name]
+    # def get_enum_value(self, name: str) -> Tuple[str, str]:
+    #     return self._enum_values[name]
     
-    def enum_value_exists(self, name: str) -> bool:
-        return name in self._enum_values
+    # def enum_value_exists(self, name: str) -> bool:
+    #     return name in self._enum_values
 
     def print(self) -> None:
         print(f'Agent {self.name}')
-        for float_param in self.floats.values():
-            float_param.print()
+        for init_float_param in self.init_floats.values():
+            init_float_param.print()
+        for dist_normal_float_param in self.dist_normal_floats.values():
+            dist_normal_float_param.print()
         for enum_param in self.enums.values():
             enum_param.print()
         for list_param in self.lists.values():
--- op.py
+++ op.py
@@ -1,7 +1,8 @@
 from environment import Environment
 from state import State
 from agent import Agent
-from param import EnumParam, FloatParam, ListParam
+from param import EnumParam, ListParam, InitFloatParam, DistNormalFloatParam
+from typing import List
 
 
 def op_ENVIRONMENT(state: State) -> None:
@@ -38,55 +39,19 @@ def op_EAGENT(state: State) -> None:
             state.panic('Incorrect operation: EAGENT')
 
 
-def op_PRM(state: State, name: str, category: str) -> None:
-    match state.in_environment, state.in_agent:
-        case True, True if not state.last_agent.param_exists(name):
-            match category:
-                case 'float':
-                    state.last_agent.add_float(FloatParam(name))
-                case 'enum':
-                    state.last_agent.add_enum(EnumParam(name))
-                case 'list':
+def op_PRM(state: State, name: str, category: str, args: List[str]) -> None:
+    match state.in_agent:
+        case True if not state.last_agent.param_exists(name):
+            match category, args:
+                case 'float', ['init', val]:
+                    state.last_agent.add_init_float(InitFloatParam(name, val))
+                case 'float', ['dist_normal', mean, std_dev]:
+                    state.last_agent.add_dist_normal_float(DistNormalFloatParam(name, mean, std_dev))
+                case 'list', ['conn_list' | 'msg_list']:
                     state.last_agent.add_list(ListParam(name))
+                case 'enum', enums:
+                    state.last_agent.add_enum(EnumParam(name, enums))
                 case _:
-                    state.panic(f'Incorrect operation: PRM {name} {category}')
+                    state.panic(f'Incorrect operation: PRM {name} {category} {args}')
         case _:
-            state.panic(f'Incorrect operation: PRM {name} {category}')
-
-
-def op_SPRM(state: State, name: str, subcategory: str) -> None:
-    match state.in_environment, state.in_agent:
-        case True, True if state.last_agent.param_exists(name):
-            match state.last_agent.get_param(name).category:
-                case 'float' if subcategory in ('init', 'dist_normal'):
-                    state.last_agent.get_param(name).subcategory = subcategory
-                case 'enum' if subcategory in ('enum_init', 'enum_percent'):
-                    state.last_agent.get_param(name).subcategory = subcategory
-                case 'list' if subcategory in ('conn_list', 'msg_list'):
-                    state.last_agent.get_param(name).subcategory = subcategory
-                case _:
-                    state.panic(f'Incorrect operation: SPRM {name} {subcategory}')
-        case _:
-            state.panic(f'Incorrect operation: SPRM {name} {subcategory}')
-            
-
-def op_EVAL(state: State, name: str, value: str) -> None:
-    match state.in_environment, state.in_agent:
-        case True, True if not state.last_agent.enum_value_exists(name):
-            state.last_agent.add_enum_value(name, value)
-        case _:
-            state.panic(f'Incorrect operation: EVAL {name} {value}')
-
-
-def op_AARG(state: State, name: str, value: str) -> None:
-    match state.in_environment, state.in_agent:
-        case True, True if state.last_agent.param_exists(name):
-            match state.last_agent.get_param(name).category:
-                case 'float':
-                    state.last_agent.get_param(name).add_subcategory_arg(value)
-                case 'enum' if state.last_agent.enum_value_exists(value):
-                    state.last_agent.get_param(name).add_subcategory_arg(state.last_agent.get_enum_value(value))
-                case _:
-                    state.panic(f'Incorrect operation: AARG {name} {value}')
-        case _:
-            state.panic(f'Incorrect operation: AARG {name} {value}')
+            state.panic(f'Incorrect operation: PRM {name} {category} {args}')
