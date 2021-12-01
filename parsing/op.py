e355e67847f2cc270849ad271352465e109fdee2
madpeh <48161600+madpeh@users.noreply.github.com>
2021-12-01 23:46:50 +0100

    Change pattern matching to require statements

--- parsing/op.py
+++ parsing/op.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from intermediate.action import Action, Declaration, GreaterThan, LessThanOrEqual, Multiply
+from intermediate.action import (Action, Declaration, GreaterThan,
+                                 LessThanOrEqual, Multiply, Subtract)
 from intermediate.agent import Agent
 from intermediate.behaviour import Behaviour
 from intermediate.environment import Environment
@@ -10,16 +11,7 @@ from parsing.state import State
 from utils.validation import is_float, is_valid_enum_list
 
 
-def op_ENVIRONMENT(state: State) -> None:
-    # match state.in_environment:
-    #     case False:
-    #         state.in_environment = True
-    #         state.add_environment(Environment())
-    #     case _:
-    #         state.panic('Incorrect operation: ENVIRONMENT')
-            
-    # state.require_not_in(state.in_environment, 'Already inside environment')
-    
+def op_ENVIRONMENT(state: State) -> None:    
     state.require_not(state.in_environment, 'Already inside environment')
     
     state.in_environment = True
@@ -27,32 +19,13 @@ def op_ENVIRONMENT(state: State) -> None:
 
 
 def op_EENVIRONMENT(state: State) -> None:
-    # match state.in_environment, state.in_agent:
-    #     case True, False:
-    #         state.in_environment = False
-    #     case _:
-    #         state.panic('Incorrect operation: EENVIRONMENT')
-            
-    # state.require_in(state.in_environment, 'Not inside environment')
-    # state.require_not_in(state.in_agent, 'Inside agent')
     state.require(state.in_environment, 'Not inside environment')
     state.require_not(state.in_agent, 'Inside agent')
     
     state.in_environment = False
 
 
-def op_AGENT(state: State, name: str) -> None:
-    # match state.in_environment, state.in_agent:
-    #     case True, False if not state.last_environment.agent_exists(name):
-    #         state.in_agent = True
-    #         state.last_environment.add_agent(Agent(name))
-    #     case _:
-    #         state.panic(f'Incorrect operation: AGENT {name}')
-            
-    # state.require_in(state.in_environment, 'Not inside environment')
-    # state.require_not_in(state.in_agent, 'Already inside agent')
-    # state.require_agent_doesnt_exist(name, f'Agent {name} already exists')
-    
+def op_AGENT(state: State, name: str) -> None:    
     state.require(state.in_environment, 'Not inside environment')
     state.require_not(state.in_agent, 'Already inside agent')
     state.require_not(state.last_environment.agent_exists(name), f'Agent {name} already exists')
@@ -61,44 +34,14 @@ def op_AGENT(state: State, name: str) -> None:
     state.last_environment.add_agent(Agent(name))
 
 
-
-def op_EAGENT(state: State) -> None:
-    # match state.in_agent, state.in_behaviour:
-    #     case True, False:
-    #         state.in_agent = False
-    #     case _:
-    #         state.panic('Incorrect operation: EAGENT')
-            
-    # state.require_in(state.in_agent, 'Not inside agent')
-    # state.require_not_in(state.in_behaviour, 'Inside behaviour')
-    
+def op_EAGENT(state: State) -> None:    
     state.require(state.in_agent, 'Not inside agent')
     state.require_not(state.in_behaviour, 'Inside behaviour')
     
     state.in_agent = False
 
 
-def op_PRM(state: State, name: str, category: str, args: List[str]) -> None:
-    # match state.in_agent, state.in_behaviour:
-    #     case True, False if not state.last_agent.param_exists(name):
-    #         match category, args:
-    #             case 'float', ['init', val] if is_float(val):
-    #                 state.last_agent.add_init_float(InitFloatParam(name, val))
-    #             case 'float', ['dist_normal', mean, std_dev] if is_float(mean) and is_float(std_dev):
-    #                 state.last_agent.add_dist_normal_float(DistNormalFloatParam(name, mean, std_dev))
-    #             case 'list', ['conn_list' | 'msg_list']:
-    #                 state.last_agent.add_list(ListParam(name))
-    #             case 'enum', enums if is_valid_enum_list(enums):
-    #                 state.last_agent.add_enum(EnumParam(name, enums))
-    #             case _:
-    #                 state.panic(f'Incorrect operation: PRM {name} {category} {args}')
-    #     case _:
-    #         state.panic(f'Incorrect operation: PRM {name} {category} {args}')
-
-    # state.require_in(state.in_agent, 'Not inside agent')
-    # state.require_not_in(state.in_behaviour, 'Cannot define agent parameters inside behaviour')
-    # state.require_agent_param_doesnt_exist(name, f'Parameter {name} already exists inside agent')
-    
+def op_PRM(state: State, name: str, category: str, args: List[str]) -> None:    
     state.require(state.in_agent, 'Not inside agent')
     state.require_not(state.in_behaviour, 'Cannot define agent parameters inside behaviour')
     state.require_not(state.last_agent.param_exists(name), f'Parameter {name} already exists inside agent')
@@ -124,18 +67,7 @@ def op_PRM(state: State, name: str, category: str, args: List[str]) -> None:
             state.panic(f'Incorrect operation: PRM {name} {category} {args}')
     
 
-def op_SETUPBEHAV(state: State, name: str):
-    # match state.in_agent, state.in_behaviour:
-    #     case True, False if not state.last_agent.behaviour_exists(name):
-    #         state.last_agent.add_setup_behaviour(Behaviour(name))
-    #         state.in_behaviour = True
-    #     case _:
-    #         state.panic(f'Incorrect operation: SETUPBEHAV {name}')
-            
-    # state.require_in(state.in_agent, 'Not inside agent')
-    # state.require_not_in(state.in_behaviour, 'Already inside behaviour')
-    # state.require_agent_behaviour_doesnt_exist(name, f'Behaviour {name} already exists in agent')
-    
+def op_SETUPBEHAV(state: State, name: str):    
     state.require(state.in_agent, 'Not inside agent')
     state.require_not(state.in_behaviour, 'Already inside behaviour')
     state.require_not(state.last_agent.behaviour_exists(name), f'Behaviour {name} already exists in agent')
@@ -144,13 +76,7 @@ def op_SETUPBEHAV(state: State, name: str):
     state.last_agent.add_setup_behaviour(Behaviour(name))
     
 
-def op_EBEHAV(state: State):
-    # match state.in_behaviour, state.in_action:
-    #     case True, False:
-    #         state.in_behaviour = False
-    #     case _:
-    #         state.panic('Incorrect operation: EBEHAV')
-            
+def op_EBEHAV(state: State):            
     state.require(state.in_behaviour, 'Not inside behaviour')
     state.require_not(state.in_action, 'Inside action')
     
@@ -158,13 +84,6 @@ def op_EBEHAV(state: State):
     
 
 def op_ACTION(state: State, name: str):
-    # match state.in_behaviour, state.in_action:
-    #     case True, False if not state.last_behaviour.action_exists(name):
-    #         state.last_agent.last_behaviour.add_action(Action(name, state.last_agent.param_names))
-    #         state.in_action = True
-    #     case _:
-    #         state.panic(f'Incorrect operation: ACTION {name}')
-
     state.require(state.in_behaviour, 'Not inside behaviour')
     state.require_not(state.in_action, 'Already inside action')
     state.require_not(state.last_behaviour.action_exists(name), f'Action {name} already exists in behaviour')
@@ -173,41 +92,21 @@ def op_ACTION(state: State, name: str):
     state.last_agent.last_behaviour.add_action(Action(name, state.last_agent.param_names))
             
     
-def op_EACTION(state: State):
-    # match state.in_action, state.nested_blocks_count:
-    #     case True, 0:
-    #         state.in_action = False
-    #     case _:
-    #         state.panic('Incorrect operation: EACTION')
-            
+def op_EACTION(state: State):            
     state.require(state.in_action, 'Not inside action')
     state.require(state.nested_blocks_count == 0, 'There are unclosed blocks')
     
     state.in_action = False
 
 
-def op_DECL(state: State, name: str, value: str):
-    # match state.in_action:
-    #     case True if not state.last_action.is_name_in_scope(name):
-    #         state.last_action.add_declaration(Declaration(name, value))
-    #     case _:
-    #         state.panic(f'Incorrect operation: DECL {name} {value}')
-            
+def op_DECL(state: State, name: str, value: str):            
     state.require(state.in_action, 'Not inside action')
     state.require_not(state.last_action.is_name_in_scope(name), f'{name} already in scope')
     
     state.last_action.add_declaration(Declaration(name, value))
 
 
-def op_GT(state: State, arg1: str, arg2: str):
-    # match state.in_action:
-    #     case True if state.last_action.is_name_in_scope(arg1) and state.last_action.is_name_in_scope(arg2):
-    #         state.last_action.add_expression(GreaterThan(arg1, arg2))
-    #         state.last_action.start_block()
-    #         state.nested_blocks_count += 1
-    #     case _:
-    #         state.panic(f'Incorrect operation: GT {arg1} {arg2}')
-            
+def op_GT(state: State, arg1: str, arg2: str):            
     state.require(state.in_action, 'Not inside action')
     state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
     state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
@@ -217,16 +116,7 @@ def op_GT(state: State, arg1: str, arg2: str):
     state.nested_blocks_count += 1
     
 
-
-def op_LTE(state: State, arg1: str, arg2: str):
-    # match state.in_action:
-    #     case True if state.last_action.is_name_in_scope(arg1) and state.last_action.is_name_in_scope(arg2):
-    #         state.last_action.add_expression(LessThanOrEqual(arg1, arg2))
-    #         state.last_action.start_block()
-    #         state.nested_blocks_count += 1
-    #     case _:
-    #         state.panic(f'Incorrect operation: LTE {arg1} {arg2}')
-    
+def op_LTE(state: State, arg1: str, arg2: str):    
     state.require(state.in_action, 'Not inside action')     
     state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
     state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
@@ -236,15 +126,7 @@ def op_LTE(state: State, arg1: str, arg2: str):
     state.nested_blocks_count += 1
     
 
-
-def op_EBLOCK(state: State):
-    # match state.in_action:
-    #     case True if state.nested_blocks_count > 0:
-    #         state.last_action.end_block()
-    #         state.nested_blocks_count -= 1
-    #     case _:
-    #         state.panic(f'Incorrect operation: EBLOCK')
-            
+def op_EBLOCK(state: State):            
     state.require(state.in_action, 'Not inside action')
     state.require(state.nested_blocks_count > 0, 'No more blocks to close')
     
@@ -252,12 +134,7 @@ def op_EBLOCK(state: State):
     state.nested_blocks_count -= 1
 
     
-def op_MULT(state: State, arg1: str, arg2: str):
-    # if state.in_action and state.last_action.is_name_in_scope(arg1) and state.last_action.is_name_in_scope(arg2) and state.last_agent.is_immutable(arg1):
-    #     state.last_action.add_expression(Multiply(arg1, arg2))
-    # else:
-    #     state.panic(f'Incorrect operation: MULT {arg1} {arg2}')
-        
+def op_MULT(state: State, arg1: str, arg2: str):        
     state.require(state.in_action, 'Not inside action')
     state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
     state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
@@ -267,4 +144,9 @@ def op_MULT(state: State, arg1: str, arg2: str):
 
 
 def op_SUBT(state: State, arg1: str, arg2: str):
-    ...
+    state.require(state.in_action, 'Not inside action')
+    state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
+    state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
+    state.require_not(state.last_agent.is_immutable(arg1), f'{arg1} is immutable')
+    
+    state.last_action.add_expression(Subtract(arg1, arg2))
