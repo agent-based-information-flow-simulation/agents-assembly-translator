61ae6b2c3ab7f5eaf8a168934c92033aa9083230
madpeh <48161600+madpeh@users.noreply.github.com>
2021-12-02 02:26:03 +0100

    Add more meaningful messages

--- parsing/op.py
+++ parsing/op.py
@@ -12,111 +12,111 @@ from utils.validation import is_float, is_valid_enum_list
 
 
 def op_ENVIRONMENT(state: State) -> None:    
-    state.require_not(state.in_environment, 'Already inside environment')
+    state.require_not(state.in_environment, 'Already inside an environment. Try defining new agents (AGENT).')
     
     state.in_environment = True
     state.add_environment(Environment())
 
 
 def op_EENVIRONMENT(state: State) -> None:
-    state.require(state.in_environment, 'Not inside environment')
-    state.require_not(state.in_agent, 'Inside agent')
+    state.require(state.in_environment, 'Not inside any environment.')
+    state.require_not(state.in_agent, 'Cannot end an environment inside an agent. First end current agent (EAGENT).')
     
     state.in_environment = False
 
 
 def op_AGENT(state: State, name: str) -> None:    
-    state.require(state.in_environment, 'Not inside environment')
-    state.require_not(state.in_agent, 'Already inside agent')
-    state.require_not(state.last_environment.agent_exists(name), f'Agent {name} already exists')
+    state.require(state.in_environment, 'Not inside any environment. Define agents inside environments (ENVIRONMENT).')
+    state.require_not(state.in_agent, 'Already inside an agent. First end current agent (EAGENT).')
+    state.require_not(state.last_environment.agent_exists(name), f'Agent {name} already exists in the current environment.')
     
     state.in_agent = True
     state.last_environment.add_agent(Agent(name))
 
 
 def op_EAGENT(state: State) -> None:    
-    state.require(state.in_agent, 'Not inside agent')
-    state.require_not(state.in_behaviour, 'Inside behaviour')
+    state.require(state.in_agent, 'Not inside any agent. Try defining new agents (AGENT).')
+    state.require_not(state.in_behaviour, 'Cannot end an agent inside a behaviour. First end current behaviour (EBEHAV).')
     
     state.in_agent = False
 
 
 def op_PRM(state: State, name: str, category: str, args: List[str]) -> None:    
-    state.require(state.in_agent, 'Not inside agent')
-    state.require_not(state.in_behaviour, 'Cannot define agent parameters inside behaviour')
-    state.require_not(state.last_agent.param_exists(name), f'Parameter {name} already exists inside agent')
+    state.require(state.in_agent, 'Not inside any agent. Try defining new agents (AGENT).')
+    state.require_not(state.in_behaviour, 'Cannot define agent parameters inside a behaviour. They must appear after AGENT.')
+    state.require_not(state.last_agent.param_exists(name), f'Parameter {name} already exists inside current agent.')
     
     match category, args:
         case 'float', ['init', val]:
-            state.require(is_float(val), f'{val} is not valid float')
+            state.require(is_float(val), f'{val} is not a valid float.')
             state.last_agent.add_init_float(InitFloatParam(name, val))
             
         case 'float', ['dist_normal', mean, std_dev]:
-            state.require(is_float(mean), f'{mean} is not valid float')
-            state.require(is_float(std_dev), f'{std_dev} is not valid float')
+            state.require(is_float(mean), f'{mean} is not a valid float.')
+            state.require(is_float(std_dev), f'{std_dev} is not a valid float.')
             state.last_agent.add_dist_normal_float(DistNormalFloatParam(name, mean, std_dev))
             
         case 'list', ['conn_list' | 'msg_list']:
             state.last_agent.add_list(ListParam(name))
             
         case 'enum', enums:
-            state.require(is_valid_enum_list(enums), f'{enums} is not valid enum list')
+            state.require(is_valid_enum_list(enums), f'{enums} is not a valid enum list. The correct pattern is [name, percent, ...], where percent(s) sum up to 100 (+/- 1).')
             state.last_agent.add_enum(EnumParam(name, enums))
             
         case _:
             state.panic(f'Incorrect operation: PRM {name} {category} {args}')
-    
+
 
 def op_SETUPBEHAV(state: State, name: str) -> None:    
-    state.require(state.in_agent, 'Not inside agent')
-    state.require_not(state.in_behaviour, 'Already inside behaviour')
-    state.require_not(state.last_agent.behaviour_exists(name), f'Behaviour {name} already exists in agent')
+    state.require(state.in_agent, 'Cannot define behaviours outside agents. Try defining new agents (AGENT).')
+    state.require_not(state.in_behaviour, 'Cannot define behaviours inside other behaviours. First end current behaviour (EBEHAV).')
+    state.require_not(state.last_agent.behaviour_exists(name), f'Behaviour {name} already exists in current agent.')
     
     state.in_behaviour = True
     state.last_agent.add_setup_behaviour(Behaviour(name))
-    
+
 
 def op_EBEHAV(state: State) -> None:            
-    state.require(state.in_behaviour, 'Not inside behaviour')
-    state.require_not(state.in_action, 'Inside action')
+    state.require(state.in_behaviour, 'Not inside any behaviour.')
+    state.require_not(state.in_action, 'Cannot end behaviours inside actions. First try ending current action (EACTION).')
     
     state.in_behaviour = False
-    
+
 
 def op_ACTION(state: State, name: str) -> None:
-    state.require(state.in_behaviour, 'Not inside behaviour')
-    state.require_not(state.in_action, 'Already inside action')
-    state.require_not(state.last_behaviour.action_exists(name), f'Action {name} already exists in behaviour')
+    state.require(state.in_behaviour, 'Actions must be definied inside behaviours (BEHAV).')
+    state.require_not(state.in_action, 'Cannot define an action in another action. First try finishing the current one (EACTION).')
+    state.require_not(state.last_behaviour.action_exists(name), f'Action {name} already exists in current behaviour.')
     
     state.in_action = True
     state.last_behaviour.add_action(Action(name, state.last_agent.param_names))
-            
-    
+
+
 def op_EACTION(state: State) -> None:            
-    state.require(state.in_action, 'Not inside action')
-    state.require(state.last_action._nested_blocks_count == 0, 'There are unclosed blocks')
+    state.require(state.in_action, 'Not inside any action.')
+    state.require(state.last_action._nested_blocks_count == 0, 'There are unclosed blocks. Try closing them with EBLOCK.')
     
     state.in_action = False
 
 
 def op_DECL(state: State, name: str, value: str) -> None:            
-    state.require(state.in_action, 'Not inside action')
-    state.require_not(state.last_action.is_name_in_scope(name), f'{name} already in scope')
+    state.require(state.in_action, 'Cannot declare variables outside actions.')
+    state.require_not(state.last_action.is_name_in_scope(name), f'{name} is already in current scope.')
     
     state.last_action.add_declaration(Declaration(name, value))
 
 
 def op_EBLOCK(state: State) -> None:            
-    state.require(state.in_action, 'Not inside action')
-    state.require(state.last_action._nested_blocks_count > 0, 'No more blocks to close')
+    state.require(state.in_action, 'Cannot end blocks outside actions.')
+    state.require(state.last_action._nested_blocks_count > 0, 'No more blocks to close. Try removing this statement.')
     
     state.last_action.end_block()
-    
+
     
 def handle_non_mutating_statement(state: State, op: str, arg1: str, arg2: str) -> None:
-    state.require(state.in_action, 'Not inside action')     
-    state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
-    state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
+    state.require(state.in_action, 'Statements must be defined inside an action.')     
+    state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} is not in the current scope.')
+    state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} is not in the current scope.')
     
     match op:
         case 'GT':
@@ -130,10 +130,10 @@ def handle_non_mutating_statement(state: State, op: str, arg1: str, arg2: str) -
 
 
 def handle_mutating_statement(state: State, op: str, arg1: str, arg2: str) -> None:
-    state.require(state.in_action, 'Not inside action')
-    state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} not in scope')
-    state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} not in scope')
-    state.require(state.last_agent.is_mutable(arg1), f'{arg1} is immutable')
+    state.require(state.in_action, 'Statements must be defined inside an action.')
+    state.require(state.last_action.is_name_in_scope(arg1), f'{arg1} is not in the current scope.')
+    state.require(state.last_action.is_name_in_scope(arg2), f'{arg2} is not in the current scope.')
+    state.require(state.last_agent.is_mutable(arg1), f'{arg1} is immutable.')
     
     match op:
         case 'MULT':
