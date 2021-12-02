254fab3fa22156e7ad14468eea3a2175e448e494
madpeh <48161600+madpeh@users.noreply.github.com>
2021-12-02 05:24:59 +0100

    Add spade for for setup behaviour

--- generating/spade.py
+++ generating/spade.py
@@ -1,6 +1,8 @@
 from typing import List
+from intermediate.action import Block, GreaterThan, LessThanOrEqual, Multiply, Subtract, VariableValue
 
 from intermediate.agent import Agent
+from intermediate.behaviour import Behaviour
 from parsing.state import ParsedData
 
 class SpadeCode:
@@ -11,15 +13,17 @@ class SpadeCode:
         self.code_lines: List[str] = []
         self.add_required_imports()
         for agent in parsed_data.agents:
+            self.add_lines('', 2)
             self.generate_agent(agent)
         for message in parsed_data.messages:
+            self.add_lines('', 2)
             self.generate_message(message)
-        
+    
+    ### COMMON ###
     def add_required_imports(self) -> List[str]:
         self.add_line(f'import random as r')
         self.add_line(f'import spade')
         self.add_line(f'from numpy.random import normal')
-        self.add_line('')
         
     def indent_left(self) -> None:
         self.indent -= 1
@@ -29,33 +33,90 @@ class SpadeCode:
         
     def add_line(self, line: str) -> None:
         self.code_lines.append(SpadeCode.INDENT_SIZE * self.indent * ' ' + line + '\n')
-
-    def generate_agent(self, agent: Agent) -> None:
-        self.add_line('')
-        self.add_line(f'class {agent.name}(spade.agent.Agent):')
-        self.indent_right()
+        
+    def add_lines(self, line: str, count: int):
+        for _ in range(count):
+            self.add_line(line)
+    
+    ### AGENT ###
+    def add_agent_constructor(self, agent: Agent) -> None:
         self.add_line('def __init__(self, jid, password, location, connections):')
         self.indent_right()
         self.add_line('super().__init__(jid, password, verify_security=False)')
-        
-        for param in agent.init_floats.values():
-            self.add_line(f'self.{param.name} = {param.value}')
-            
-        for param in agent.dist_normal_floats.values():
-            self.add_line(f'self.{param.name} = normal({param.mean}, {param.std_dev})')
-        
-        for param in agent.enums.values():            
+        for init_float_param in agent.init_floats.values():
+            self.add_line(f'self.{init_float_param.name} = {init_float_param.value}')
+        for dist_normal_float_param in agent.dist_normal_floats.values():
+            self.add_line(f'self.{dist_normal_float_param.name} = normal({dist_normal_float_param.mean}, {dist_normal_float_param.std_dev})')
+        for enum_param in agent.enums.values():            
             names = []
             weights = []
-            for name, weight in param.enums:
+            for name, weight in enum_param.enums:
                 names.append(f'\"{name}\"')
                 weights.append(weight)
             names = f'[{", ".join(names)}]'
             weights = f'[{", ".join(weights)}]'
-            self.add_line(f'self.{param.name} = r.choices({names}, {weights})[0]')
+            self.add_line(f'self.{enum_param.name} = r.choices({names}, {weights})[0]')
+        for list_param in agent.lists.values():
+            self.add_line(f'self.{list_param.name} = []')
+        self.indent_left()
         
-        for param in agent.lists.values():
-            self.add_line(f'self.{param.name} = []')
+    def add_agent_setup(self, agent: Agent) -> None:
+        self.add_line('def setup(self):')
+        self.indent_right()
+        for setup_behaviour in agent.setup_behaviours:
+            self.add_line(f'self.add_behaviour(self.{setup_behaviour}())')
+        self.indent_left()
             
+    def add_agent_behaviour(self, behaviour: Behaviour, behaviour_type: str) -> None:
+        self.add_line(f'class {behaviour.name}({behaviour_type}):')
+        self.indent_right()
+        for action in behaviour.actions.values():
+            self.add_line(f'async def {action.name}(self):')
+            self.indent_right()
+            self.add_block(action.main_block)
+            self.indent_left()
+        self.add_line('')
+        self.add_line('async def run(self):')
+        self.indent_right()
+        for action_name in behaviour.actions:
+            self.add_line(f'self.{action_name}()')
+        self.indent_left()
+        self.indent_left()
+        
+    def is_from_agent(self, var: VariableValue) -> str:
+        return 'self.agent.' if var.is_value_from_agent else ''
+        
+    def add_block(self, block: Block) -> None:
+        for declaration in block.declarations.values():
+            self.add_line(f'{declaration.name} = {self.is_from_agent(declaration)}{declaration.value}')
+        for statement in block.statements:
+            if isinstance(statement, Block):
+                self.indent_right()
+                self.add_block(statement)
+                self.indent_left()
+            else:
+                arg1 = self.is_from_agent(statement.arg1) + statement.arg1.value
+                arg2 = self.is_from_agent(statement.arg2) + statement.arg2.value
+                if isinstance(statement, GreaterThan):
+                    self.add_line(f'if {arg1} > {arg2}:')
+                elif isinstance(statement, LessThanOrEqual):
+                    self.add_line(f'if {arg1} <= {arg2}:')
+                elif isinstance(statement, Subtract):
+                    self.add_line(f'{arg1} -= {arg2}')
+                elif isinstance(statement, Multiply):
+                    self.add_line(f'{arg1} *= {arg2}')
+
+    def generate_agent(self, agent: Agent) -> None:
+        self.add_line(f'class {agent.name}(spade.agent.Agent):')
+        self.indent_right()
+        self.add_agent_constructor(agent)
+        self.add_line('')
+        self.add_agent_setup(agent)
+        self.add_line('')
+        for setup_behaviour in agent.setup_behaviours.values():
+            self.add_agent_behaviour(setup_behaviour, 'spade.behaviour.OneShotBehaviour')
+        self.indent_left()
+    
+    ### MESSAGE ###
     def generate_message(agent: Agent) -> None:
         ...
