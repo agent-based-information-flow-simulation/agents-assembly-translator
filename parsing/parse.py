b1f6e9b67d5092fbb29d300e8631951749639101
madpeh <48161600+madpeh@users.noreply.github.com>
2021-12-06 04:58:02 +0100

    Add remaining ops

--- parsing/parse.py
+++ parsing/parse.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
-from parsing.op import (handle_math_statement,
+from parsing.op import (handle_list_inclusion, handle_list_modification, handle_math_statement,
                         handle_ordered_conditional_statement,
                         handle_unordered_conditional_statement, op_ACTION,
-                        op_ADDELEM, op_AGENT, op_DECL, op_EACTION, op_EAGENT,
-                        op_EBEHAV, op_EBLOCK, op_EMESSAGE, op_MESSAGE, op_SET, op_SETUPBEHAV, op_agent_PRM, op_message_PRM)
+                        op_AGENT, op_agent_PRM, op_BEHAV, op_CLR,
+                        op_DECL, op_EACTION, op_EAGENT, op_EBEHAV, op_EBLOCK,
+                        op_EMESSAGE, op_LEN, op_MESSAGE, op_message_PRM,
+                        op_SEND, op_SET, op_SUBS)
 from parsing.state import State
 
 if TYPE_CHECKING:
@@ -23,8 +25,8 @@ def parse_lines(lines: List[str], debug: bool) -> ParsedData:
             case [ 'EAGENT' ]:
                 op_EAGENT(state)
                 
-            case [ 'MESSAGE', name ]:
-                op_MESSAGE(state, name)
+            case [ 'MESSAGE', name, performative ]:
+                op_MESSAGE(state, name, performative)
             
             case [ 'EMESSAGE' ]:
                 op_EMESSAGE(state)
@@ -35,14 +37,14 @@ def parse_lines(lines: List[str], debug: bool) -> ParsedData:
             case [ 'PRM', name, category, *args ]:
                 op_agent_PRM(state, name, category, args)
                 
-            case [ 'SETUPBEHAV', name ]:
-                op_SETUPBEHAV(state, name)
+            case [ 'BEHAV', name, category, *args ]:
+                op_BEHAV(state, name, category, args)
                 
             case [ 'EBEHAV' ]:
                 op_EBEHAV(state)
                 
-            case [ 'ACTION', name ]:
-                op_ACTION(state, name)
+            case [ 'ACTION', name, category, *args ]:
+                op_ACTION(state, name, category, args)
                 
             case [ 'EACTION' ]:
                 op_EACTION(state)
@@ -53,17 +55,32 @@ def parse_lines(lines: List[str], debug: bool) -> ParsedData:
             case [ 'EBLOCK' ]:
                 op_EBLOCK(state)
                 
-            case [ 'IE' | 'INE' | 'WE' | 'WNE' as op, arg1, arg2 ]:
+            case [ 'IEQ' | 'INEQ' | 'WEQ' | 'WNEQ' as op, arg1, arg2 ]:
                 handle_unordered_conditional_statement(state, op, arg1, arg2)
                 
-            case [ 'IGT' | 'IGTE' | 'ILT' | 'ILTE' | 'WGT' | 'WGTE' | 'WLT' | 'WLTE' as op, arg1, arg2 ]:
+            case [ 'IGT' | 'IGTEQ' | 'ILT' | 'ILTEQ' | 'WGT' | 'WGTEQ' | 'WLT' | 'WLTEQ' as op, arg1, arg2 ]:
                 handle_ordered_conditional_statement(state, op, arg1, arg2)
                 
             case [ 'ADD' | 'SUBT' | 'MULT' | 'DIV' as op, arg1, arg2 ]:
                 handle_math_statement(state, op, arg1, arg2)
                 
-            case [ 'ADDELEM' | 'ADDE', arg1, arg2 ]:
-                op_ADDELEM(state, arg1, arg2)
+            case [ 'ADDE' | 'REME' as op, list_, element ]:
+                handle_list_modification(state, op, list_, element)
+                
+            case [ 'LEN', result, list_ ]:
+                op_LEN(state, result, list_)
+                
+            case [ 'CLR', list_ ]:
+                op_CLR(state, list_)
+                
+            case [ 'IN' | 'NIN' as op, list_, element ]:
+                handle_list_inclusion(state, list_, element)
+                
+            case [ 'SEND', rcv_list ]:
+                op_SEND(state, rcv_list)
+                
+            case [ 'SUBS', dst_list, src_list, num ]:
+                op_SUBS(state, dst_list, src_list, num)
                 
             case [ 'SET', arg1, arg2 ]:
                 op_SET(state, arg1, arg2)
