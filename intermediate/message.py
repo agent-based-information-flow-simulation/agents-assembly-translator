b0c4a0db9e8ee1e4237528c092cb8013a83bf7e3
madpeh <48161600+madpeh@users.noreply.github.com>
2021-12-06 04:57:14 +0100

    Add new message representation

--- intermediate/message.py
+++ intermediate/message.py
@@ -7,15 +7,31 @@ if TYPE_CHECKING:
 
 
 class Message:
-    RESERVED_PARAMS = [ 'sender', 'type', 'performative' ]
+    RESERVED_CONNECTION_PARAMS = [ 'sender' ]
+    RESERVED_TYPE_PARAMS = [ 'type', 'performative' ]
     
-    def __init__(self, _type: str):
-        self.type: str = _type
+    def __init__(self, msg_type: str, msg_performative: str):
+        self.type: str = msg_type
+        self.performative: str = msg_performative
         self.float_params: Dict[str, MessageFloatParam] = {}
         
     @property
     def param_names(self) -> List[str]:
-        return [ *Message.RESERVED_PARAMS, *list(self.float_params) ]
+        return [ *Message.RESERVED_CONNECTION_PARAMS,
+                 *Message.RESERVED_TYPE_PARAMS,
+                 *list(self.float_params) ]
+    
+    @property
+    def are_all_params_set(self) -> bool:
+        return all([float_param.is_value_set for float_param in self.float_params.values()])
+    
+    @property
+    def unset_params(self) -> List[str]:
+        unset_params: List[str] = []
+        for name, float_param in self.float_params.items():
+            if not float_param.is_value_set:
+                unset_params.append(name)
+        return unset_params
     
     def param_exists(self, name: str) -> bool:
         return name in self.param_names
@@ -24,6 +40,6 @@ class Message:
         self.float_params[float_param.name] = float_param
         
     def print(self) -> None:
-        print(f'Message {self.type}')
+        print(f'Message {self.type}/{self.performative}')
         for float_params in self.float_params.values():
             float_params.print()
