418020668e73e51d623199076854f5a9fc97c522
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-26 19:42:05 +0100

    Add type checking to block instructions

--- aasm/intermediate/instruction.py
+++ aasm/intermediate/instruction.py
@@ -7,281 +7,328 @@ if TYPE_CHECKING:
 
 
 class Instruction:
-    def __init__(self, **kwargs: Argument):
-        for key in kwargs:
-            self.__dict__[key] = kwargs[key]
-  
     def print(self) -> None:
-        print('Instruction')
-        for argument in self.__dict__.values():
-            argument.print()
+        raise NotImplementedError()
 
 
-class IfGreaterThan(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class Comparaison(Instruction):
+    def __init__(self, left: Argument, right: Argument):
+        self.left = left
+        self.right = right
+
+    def print(self) -> None:
+        print('Comparaison')
+        self.left.print()
+        self.right.print()
+
+
+class IfGreaterThan(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
  
     def print(self) -> None:
         print('IfGreaterThan')
         super().print()
 
 
-class IfGreaterThanOrEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfGreaterThanOrEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
  
     def print(self) -> None:
         print('IfGreaterThanOrEqual')
         super().print()
 
 
-class IfLessThan(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfLessThan(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('IfLessThan')
         super().print()
 
 
-class IfLessThanOrEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfLessThanOrEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('IfLessThanOrEqual')
         super().print()
 
 
-class IfEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('IfEqual')
         super().print()
 
 
-class IfNotEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfNotEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('IfNotEqual')
         super().print()
 
 
-class WhileGreaterThan(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileGreaterThan(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileGreaterThan')
         super().print()
 
 
-class WhileGreaterThanOrEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileGreaterThanOrEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileGreaterThanOrEqual')
         super().print()
 
 
-class WhileLessThan(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileLessThan(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileLessThan')
         super().print()
 
 
-class WhileLessThanOrEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileLessThanOrEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileLessThanOrEqual')
         super().print()
 
 
-class WhileEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileEqual')
         super().print()
 
 
-class WhileNotEqual(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class WhileNotEqual(Comparaison):
+    def __init__(self, left: Argument, right: Argument):
+        super().__init__(left=left, right=right)
 
     def print(self) -> None:
         print('WhileNotEqual')
         super().print()
 
 
-class Multiply(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class MathOperation(Instruction):
+    def __init__(self, dst: Argument, num: Argument):
+        self.dst = dst
+        self.num = num
+
+    def print(self) -> None:
+        print('MathOperation')
+        self.dst.print()
+        self.num.print()
+
+
+class Multiply(MathOperation):
+    def __init__(self, dst: Argument, num: Argument):
+        super().__init__(dst=dst, num=num)
 
     def print(self) -> None:
         print('Multiply')
         super().print()
 
 
-class Divide(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class Divide(MathOperation):
+    def __init__(self, dst: Argument, num: Argument):
+        super().__init__(dst=dst, num=num)
 
     def print(self) -> None:
         print('Divide')
         super().print()
 
 
-class Add(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class Add(MathOperation):
+    def __init__(self, dst: Argument, num: Argument):
+        super().__init__(dst=dst, num=num)
 
     def print(self) -> None:
         print('Add')
         super().print()
 
 
-class Subtract(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class Subtract(MathOperation):
+    def __init__(self, dst: Argument, num: Argument):
+        super().__init__(dst=dst, num=num)
 
     def print(self) -> None:
         print('Subtract')
         super().print()
 
 
-class AddElement(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class ListElementAccess(Instruction):
+    def __init__(self, list_: Argument, element: Argument):
+        self.list_ = list_
+        self.element = element
+
+    def print(self) -> None:
+        print('ListElementAccess')
+        self.list_.print()
+        self.element.print()
+
+
+class AddElement(ListElementAccess):
+    def __init__(self, list_: Argument, element: Argument):
+        super().__init__(list_=list_, element=element)
 
     def print(self) -> None:
         print('AddElement')
         super().print()
 
 
-class RemoveElement(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class RemoveElement(ListElementAccess):
+    def __init__(self, list_: Argument, element: Argument):
+        super().__init__(list_=list_, element=element)
 
     def print(self) -> None:
         print('RemoveElement')
         super().print()
 
 
-class Subset(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument, arg3: Argument):
-        super().__init__(arg1=arg1, arg2=arg2, arg3=arg3)
-
-    def print(self) -> None:
-        print('RemoveElement')
-        super().print()
-
-
-class Set(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
-
-    def print(self) -> None:
-        print('Set')
-        super().print()
-
-
-class IfInList(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfInList(ListElementAccess):
+    def __init__(self, list_: Argument, element: Argument):
+        super().__init__(list_=list_, element=element)
 
     def print(self) -> None:
         print('IfInList')
         super().print()
 
 
-class IfNotInList(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class IfNotInList(ListElementAccess):
+    def __init__(self, list_: Argument, element: Argument):
+        super().__init__(list_=list_, element=element)
 
     def print(self) -> None:
         print('IfNotInList')
         super().print()
 
 
-class Clear(Instruction):
-    def __init__(self, arg1: Argument):
-        super().__init__(arg1=arg1)
-
-    def print(self) -> None:
-        print('Clear')
-        super().print()
-
-
-class Length(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
-
-    def print(self) -> None:
-        print('Length')
-        super().print()
-
-
-class Send(Instruction):
-    def __init__(self, arg1: Argument):
-        super().__init__(arg1=arg1)
-
-    def print(self) -> None:
-        print('Send')
-        super().print()
-        
-        
 class RemoveNElements(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+    def __init__(self, list_: Argument, num: Argument):
+        self.list_ = list_
+        self.num = num
 
     def print(self) -> None:
         print('RemoveNElements')
-        super().print()
+        self.list_.print()
+        self.num.print()
 
 
-class UniformDist(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument, arg3: Argument):
-        super().__init__(arg1=arg1, arg2=arg2, arg3=arg3)
-
-    def print(self) -> None:
-        print('UniformDist')
-        super().print()
-
-
-class NormalDist(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument, arg3: Argument):
-        super().__init__(arg1=arg1, arg2=arg2, arg3=arg3)
+class Subset(Instruction):
+    def __init__(self, dst_list: Argument, src_list: Argument, num: Argument):
+        self.dst_list = dst_list
+        self.src_list = src_list
+        self.num = num
 
     def print(self) -> None:
-        print('NormalDist')
-        super().print()
+        print('Subset')
+        self.dst_list.print()
+        self.src_list.print()
+        self.num.print()
 
 
-class ExpDist(Instruction):
-    def __init__(self, arg1: Argument, arg2: Argument):
-        super().__init__(arg1=arg1, arg2=arg2)
+class Set(Instruction):
+    def __init__(self, dst: Argument, value: Argument):
+        self.dst = dst
+        self.value = value
 
     def print(self) -> None:
-        print('ExpDist')
-        super().print()
+        print('Set')
+        self.dst.print()
+        self.value.print()
+
+
+class Clear(Instruction):
+    def __init__(self, list_: Argument):
+        self.list_ = list_
+
+    def print(self) -> None:
+        print('Clear')
+        self.list_.print()
+
+
+class Length(Instruction):
+    def __init__(self, dst: Argument, list_: Argument):
+        self.dst = dst
+        self.list_ = list_
+
+    def print(self) -> None:
+        print('Length')
+        self.dst.print()
+        self.list_.print()
+
+
+class Send(Instruction):
+    def __init__(self, receivers: Argument):
+        self.receivers = receivers
+
+    def print(self) -> None:
+        print('Send')
+        self.receivers.print()
 
 
 class Round(Instruction):
-    def __init__(self, arg1: Argument):
-        super().__init__(arg1=arg1)
+    def __init__(self, dst: Argument):
+        self.dst = dst
 
     def print(self) -> None:
         print('Round')
-        super().print()
+        self.dst.print()
+
+
+class UniformDist(Instruction):
+    def __init__(self, dst: Argument, a: Argument, b: Argument):
+        self.dst = dst
+        self.a = a
+        self.b = b
+
+    def print(self) -> None:
+        print('UniformDist')
+        self.dst.print()
+        self.a.print()
+        self.b.print()
+
+
+class NormalDist(Instruction):
+    def __init__(self, dst: Argument, mean: Argument, std_dev: Argument):
+        self.dst = dst
+        self.mean = mean
+        self.std_dev = std_dev
+
+    def print(self) -> None:
+        print('NormalDist')
+        self.dst.print()
+        self.mean.print()
+        self.std_dev.print()
+
+
+class ExpDist(Instruction):
+    def __init__(self, dst: Argument, lambda_: Argument):
+        self.dst = dst
+        self.lambda_ = lambda_
+
+    def print(self) -> None:
+        print('ExpDist')
+        self.dst.print()
+        self.lambda_.print()
