5a3ca0ce0b353bef73eb64b68685b6ae3c4c7d1e
madpeh <48161600+madpeh@users.noreply.github.com>
2023-05-23 14:33:13 +0200

    Parse agent reserved connection params

--- aasm/intermediate/argument.py
+++ aasm/intermediate/argument.py
@@ -65,6 +65,10 @@ class AgentParam(ArgumentType):
     ...
 
 
+class SelfConnection(ArgumentType):
+    ...
+
+
 class Message(ArgumentType):
     ...
 
@@ -120,6 +124,9 @@ class Argument:
 
         elif self.expr in state.last_agent.RESERVED_CONNECTION_LIST_PARAMS:
             self.types.append(self.compose(List, ConnectionList, AgentParam, Mutable))
+
+        elif self.expr in state.last_agent.RESERVED_CONNECTION_PARAMS:
+            self.types.append(self.compose(Connection, AgentParam, Immutable))
 
         elif self.expr in state.last_agent.float_param_names:
             self.types.append(self.compose(Float, AgentParam, Mutable))
